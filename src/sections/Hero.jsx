863d90dde0078b54744aa14ba866e0d57f7d7c72
Asjad Nasim <asjadnasim@gmail.com>
2024-09-14 16:16:23 +0530

    Hero and Button section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -11,6 +11,7 @@ import ReactLogo from '../components/ReactLogo';
 import Cube from '../components/Cube';
 import Rings from '../components/Ring';
 import HeroCamera from '../components/HeroCamera';
+import Button from '../components/Button';
 
 const Hero = () => {
 	// const x = useControls('HackerRoom', {
@@ -99,6 +100,16 @@ const Hero = () => {
 						<directionalLight position={[10, 10, 10]} intensity={0.5} />
 					</Suspense>
 				</Canvas>
+			</div>
+
+			<div className='absolute bottom-7 left-0 right-0 w-full z-10 c-space'>
+				<a href='#contact' className='w-fit'>
+					<Button
+						name={`Let's Work Together`}
+						isBeam
+						containerClass='sm:w-fit w-full sm:min-w-96'
+					/>
+				</a>
 			</div>
 		</section>
 	);
